e8e8a086156b3b1e57143093f671ea3dd5734471
imani.devs <imaniroberts.dev@gmail.com>
2023-05-04 18:35:28 -0400

    fixed params

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import Nav from './components/Nav';
-import About from './pages/About';
-import Dashboard from './pages/Dashboard.js';
-import Stocks from './pages/Stocks';
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard.js";
+import Stocks from "./pages/Stocks";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-	return (
-		<div>
-			
-			<Nav />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/Dashboard" element={<Dashboard />} />
-				<Route path="/About" element={<About />} />
-				<Route path="/stocks/:symbol" element={<Stocks />} />
-				<Route path="/stocks" element={<Stocks />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stocks />} />
+        <Route path="/stocks" element={<Stocks />} />
+      </Routes>
+    </div>
+  );
 }
 export default App;
--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Data from '../Data' //the two dots means go up one folder
+import React from "react";
+import { useParams } from "react-router-dom";
+import Data from "../Data"; //the two dots means go up one folder
 
 const Stocks = () => {
-	const { symbol } = useParams(); // destructure the symbol from the url params
-	Data.filter((company) => company.symbol === symbol); // filter the stocks array to return only the stock that matches the symbol in the url
+  const params = useParams();
+  const symbol = params.symbol;
+  console.log(symbol);
+  console.log(Data);
+  let filteredData;
 
-	const stockList = Data.map((company, index) => ( // map over the stocks array and return a div with the stock name and symbol
-		<div key={index} className='stocklist'>  {/* use the index as the key */}
-			<h2> {company.name} </h2>
-			<h2> {company.symbol} </h2>
-			<h2> Last Price: <span className='lstprc'>{company.lastPrice}</span> </h2>
-		</div>
-	));
+  if (symbol) {
+    filteredData = Data.filter((company) => company.symbol === symbol); // filter the stocks array to return only the stock that matches the symbol in the url
+  } else {
+    filteredData = Data;
+  }
 
-	return <div>{stockList}</div>; // return the div with the list of stocks
+  const stockList = filteredData.map(
+    (
+      company,
+      index // map over the stocks array and return a div with the stock name and symbol
+    ) => (
+      <div key={index} className="stocklist">
+        {" "}
+        {/* use the index as the key */}
+        <h2> {company.name} </h2>
+        <h2> {company.symbol} </h2>
+        <h2>
+          {" "}
+          Last Price: <span className="lstprc">{company.lastPrice}</span>{" "}
+        </h2>
+      </div>
+    )
+  );
+
+  return <div>{stockList}</div>; // return the div with the list of stocks
 };
 
 export default Stocks;
